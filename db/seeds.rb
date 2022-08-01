1366e99f4c3101a701cd55cc8cfc1d689780eb5c
Njoki <kinuthiash@gmail.com>
2022-08-01 20:14:35 +0300

    create user table and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+  users = User.create([
+    { username:"mary" , email:"mary@gmail.com", password:"mary" }, 
+    { username:"joseph" , email:"joseph@gmail.com", password:"joseph" }   
+    ])
