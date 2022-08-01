3b16acc9d20c2f805ce8dc4aedaca2dde4b4b396
Njoki <kinuthiash@gmail.com>
2022-08-01 20:50:35 +0300

    add signup functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_message
+
+  def create
+    user = User.create!(user_params) 
+    session[:user_id] = user.id
+    
+    render json: user, status: :created
+  end
+
+  private
+  def user_params
+    params.permit(:username, :email, :password)
+  end
+
+  def render_unprocessable_entity_message(invalid)
+    render json: { errors: invalid.record.errors }
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :username, :password, presence: true
+  validates :email, uniqueness: true
+  validates :password, { :length => { :in => 3...8}}
 end
