a599676565d87ef615bb6b9426f4a701d41ca322
Njoki <kinuthiash@gmail.com>
2022-08-01 22:18:50 +0300

    add login functionality

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,29 @@
+class SessionsController < ApplicationController
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+  def create
+    user = User.find_by(username: params[:username])
+    if user&.authenticate(params[:password])
+      session[:user_id] = user.id
+      render json: user, status: :created
+    else
+      render json: { error: "Invalid username or password"}, status: :unauthorized
+    end
+  end
+
+  def destroy
+    session.delete :user_id
+    head :no_content
+  end
+
+  private 
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors }
+  end
+
+   def render_not_found_response
+    render json: { errors: "Not authorized" }, status: :unauthorized
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,37 @@
 class UsersController < ApplicationController
-rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_message
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
+  # create new user
   def create
     user = User.create!(user_params) 
-    session[:user_id] = user.id
-    
+    session[:user_id] = user.id    
     render json: user, status: :created
   end
 
+  # # autolog in already logged in user - not working
+  # def show    
+  #   user = User.find_by(id: session[:user_id])
+  #   if user
+  #    render json: user
+  #   else
+  #     render json: { error: "not logged in"}
+  #   end
+  # end
+  
+
+  # private routes
   private
   def user_params
     params.permit(:username, :email, :password)
   end
 
-  def render_unprocessable_entity_message(invalid)
+  def render_unprocessable_entity_response(invalid)
     render json: { errors: invalid.record.errors }
   end
+
+   def render_not_found_response
+    render json: { errors: "Not authorized" }, status: :unauthorized
+  end
+
 end
